d0a2c98c7aa8e6f3b1f49f14f9a471856cff0482
Zorgatone <zorgatone@gmail.com>
2020-09-15 19:53:46 +0200

    Matrices and Uniform Variables

--- src/tk/zorgatone/shaders/ShaderProgram.java
+++ src/tk/zorgatone/shaders/ShaderProgram.java
@@ -3,14 +3,85 @@ package tk.zorgatone.shaders;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.FloatBuffer;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
 
 public abstract class ShaderProgram {
 
   private final int programID;
   private final int vertexShaderID;
   private final int fragmentShaderID;
+
+  private static final FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
+
+  public ShaderProgram(String vertexFile, String fragmentFile) {
+    vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
+    fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
+    programID = GL20.glCreateProgram();
+
+    GL20.glAttachShader(programID, vertexShaderID);
+    GL20.glAttachShader(programID, fragmentShaderID);
+
+    bindAttributes();
+
+    GL20.glLinkProgram(programID);
+    GL20.glValidateProgram(programID);
+
+    getAllUniformLocations();
+  }
+
+  public void start() {
+    GL20.glUseProgram(programID);
+  }
+
+  public void stop() {
+    GL20.glUseProgram(0);
+  }
+
+  public void cleanUp() {
+    stop();
+
+    GL20.glDetachShader(programID, vertexShaderID);
+    GL20.glDetachShader(programID, fragmentShaderID);
+    GL20.glDeleteShader(vertexShaderID);
+    GL20.glDeleteShader(fragmentShaderID);
+    GL20.glDeleteProgram(programID);
+  }
+
+  protected abstract void bindAttributes();
+
+  protected void bindAttribute(int attribute, String variableName) {
+    GL20.glBindAttribLocation(programID, attribute, variableName);
+  }
+
+  protected abstract void getAllUniformLocations();
+
+  @SuppressWarnings("SameParameterValue")
+  protected int getUniformLocation(String uniformName) {
+    return GL20.glGetUniformLocation(programID, uniformName);
+  }
+
+  protected void loadFloat(int location, float value) {
+    GL20.glUniform1f(location, value);
+  }
+
+  protected void loadVector(int location, Vector3f vector) {
+    GL20.glUniform3f(location, vector.x, vector.y, vector.z);
+  }
+
+  protected void loadBoolean(int location, boolean value) {
+    GL20.glUniform1f(location, value ? 1.0f : 0.0f);
+  }
+
+  protected void loadMatrix(int location, Matrix4f matrix) {
+    matrix.store(matrixBuffer);
+    matrixBuffer.flip();
+    GL20.glUniformMatrix4(location, false, matrixBuffer);
+  }
 
   private static int loadShader(String file, int type) {
     StringBuilder shaderSource = new StringBuilder();
@@ -41,44 +112,6 @@ public abstract class ShaderProgram {
     }
 
     return shaderID;
-  }
-
-  public ShaderProgram(String vertexFile, String fragmentFile) {
-    vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
-    fragmentShaderID = loadShader(fragmentFile, GL20.GL_FRAGMENT_SHADER);
-    programID = GL20.glCreateProgram();
-
-    GL20.glAttachShader(programID, vertexShaderID);
-    GL20.glAttachShader(programID, fragmentShaderID);
-
-    bindAttributes();
-
-    GL20.glLinkProgram(programID);
-    GL20.glValidateProgram(programID);
-  }
-
-  public void start() {
-    GL20.glUseProgram(programID);
-  }
-
-  public void stop() {
-    GL20.glUseProgram(0);
-  }
-
-  public void cleanUp() {
-    stop();
-
-    GL20.glDetachShader(programID, vertexShaderID);
-    GL20.glDetachShader(programID, fragmentShaderID);
-    GL20.glDeleteShader(vertexShaderID);
-    GL20.glDeleteShader(fragmentShaderID);
-    GL20.glDeleteProgram(programID);
-  }
-
-  protected abstract void bindAttributes();
-
-  protected void bindAttribute(int attribute, String variableName) {
-    GL20.glBindAttribLocation(programID, attribute, variableName);
   }
 
 }
--- src/tk/zorgatone/shaders/StaticShader.java
+++ src/tk/zorgatone/shaders/StaticShader.java
@@ -1,9 +1,13 @@
 package tk.zorgatone.shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 
   private static final String VERTEX_FILE = "src/tk/zorgatone/shaders/shader.vert";
   private static final String FRAGMENT_FILE = "src/tk/zorgatone/shaders/shader.frag";
+
+  private int locationTransformationMatrix;
 
   public StaticShader() {
     super(VERTEX_FILE, FRAGMENT_FILE);
@@ -13,6 +17,15 @@ public class StaticShader extends ShaderProgram {
   protected void bindAttributes() {
     bindAttribute(0, "position");
     bindAttribute(1, "textureCoords");
+  }
+
+  @Override
+  protected void getAllUniformLocations() {
+    locationTransformationMatrix = getUniformLocation("transformationMatrix");
+  }
+
+  public void loadTransformationMatrix(Matrix4f matrix) {
+    loadMatrix(locationTransformationMatrix, matrix);
   }
 
 }
