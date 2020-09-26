8de7e7fc440629dcdd8c1add6c8a386f9d6396d9
Zorgatone <zorgatone@gmail.com>
2020-09-26 17:32:58 +0200

    Loading 3D Models from OBJ files

--- src/tk/zorgatone/engine_tester/MainGameLoop.java
+++ src/tk/zorgatone/engine_tester/MainGameLoop.java
@@ -8,6 +8,7 @@ import tk.zorgatone.models.TexturedModel;
 import tk.zorgatone.render_engine.DisplayManager;
 import tk.zorgatone.render_engine.Loader;
 import tk.zorgatone.models.RawModel;
+import tk.zorgatone.render_engine.ObjLoader;
 import tk.zorgatone.textures.ModelTexture;
 import tk.zorgatone.render_engine.Renderer;
 import tk.zorgatone.shaders.StaticShader;
@@ -21,93 +22,18 @@ public class MainGameLoop {
     try {
       DisplayManager.createDisplay();
 
-      // OpenGL expects vertices to be defined counter clockwise by default
-      float[] vertices = {
-        -0.5f, 0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, 0.5f, -0.5f,
-
-        -0.5f, 0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f,
-        0.5f, -0.5f, 0.5f,
-        0.5f, 0.5f, 0.5f,
-
-        0.5f, 0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, 0.5f,
-        0.5f, 0.5f, 0.5f,
-
-        -0.5f, 0.5f, -0.5f,
-        -0.5f, -0.5f, -0.5f,
-        -0.5f, -0.5f, 0.5f,
-        -0.5f, 0.5f, 0.5f,
-
-        -0.5f, 0.5f, 0.5f,
-        -0.5f, 0.5f, -0.5f,
-        0.5f, 0.5f, -0.5f,
-        0.5f, 0.5f, 0.5f,
-
-        -0.5f, -0.5f, 0.5f,
-        -0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, -0.5f,
-        0.5f, -0.5f, 0.5f,
-      };
-
-      float[] textureCoordinates = {
-        0, 0,
-        0, 1,
-        1, 1,
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        1, 0,
-        0, 0,
-        0, 1,
-        1, 1,
-        1, 0,
-      };
-
-      int[] indices = {
-        0, 1, 3,
-        3, 1, 2,
-        4, 5, 7,
-        7, 5, 6,
-        8, 9, 11,
-        11, 9, 10,
-        12, 13, 15,
-        15, 13, 14,
-        16, 17, 19,
-        19, 17, 18,
-        20, 21, 23,
-        23, 21, 22,
-      };
-
       Camera camera = new Camera();
 
       loader = new Loader();
-      RawModel model = loader.loadToVAO(vertices, textureCoordinates, indices);
+      RawModel model = ObjLoader.loadObjModel("monkey", loader);
 
       ModelTexture texture = new ModelTexture(
-        loader.loadTexture("image")
+        loader.loadTexture("monkeyTexture")
       );
       TexturedModel texturedModel = new TexturedModel(model, texture);
       Entity entity = new Entity(
         texturedModel,
-        new Vector3f(0.0f, 0.0f, -5.0f),
+        new Vector3f(0.0f, 0.0f, -15.0f),
         0.0f, 0.0f, 0.0f,
         1.0f
       );
@@ -118,7 +44,7 @@ public class MainGameLoop {
 
       while (!Display.isCloseRequested()) {
 //        entity.increasePosition(0.0f, 0.0f, -0.1f);
-        entity.increaseRotation(1.0f, 1.0f, 0.0f);
+        entity.increaseRotation(0.0f, 1.0f, 0.0f);
 
         camera.move();
 
